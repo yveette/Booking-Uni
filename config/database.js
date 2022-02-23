6618e8ca84b183bb22047bd2da61055982e79ad6
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-23 15:08:22 +0200

    Add database configuration.

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+// TODO load here all created models...
+require('../models/User');
+
+const dbName = 'booking';
+const connnectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+    try {
+        mongoose.connect(connnectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Database connected');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('Database error');
+            console.error(err);
+        });
+    } catch (err) {
+        console.error('Error connecting to database');
+        process.exit(1);
+    }
+
+};
